9d02f041d2cae0725107fc5a552829d9c55653c6
DmitriyB763 <mr.diy3292@gmail.com>
2022-12-17 11:23:08 +0400

    added  tests

--- src/test/java/ru/netology/domaine/data/manager/TicketManagerTest.java
+++ src/test/java/ru/netology/domaine/data/manager/TicketManagerTest.java
@@ -1,6 +1,7 @@
 package ru.netology.domaine.data.manager;
 
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import ru.netology.domaine.data.DataTicket;
 import ru.netology.domaine.data.repository.TicketRepository;
@@ -14,16 +15,36 @@ class TicketManagerTest {
     DataTicket ticket = new DataTicket(1, 10000000, "LED", "GOJ", 90);
     DataTicket ticket1 = new DataTicket(2, 700, "KUF", "OGZ", 150);
     DataTicket ticket2 = new DataTicket(3, 1500, "LED", "GOJ", 180);
+    DataTicket ticket3 = new DataTicket(4, 1500, "MMK", "GOJ", 180);
+
 
     @Test
-    void findAllAndSort() { //проверяем метод findAll  и сортировку билетов
+    void shouldfindAllAndSort() { //проверяем метод findAll  и сортировку билетов
+
         repo.addTicket(ticket);
         repo.addTicket(ticket1);
         repo.addTicket(ticket2);
+        repo.addTicket(ticket3);
 
-        DataTicket[] expexted = {ticket2, ticket};
+        DataTicket[] expected = {ticket2, ticket};
         DataTicket[] actual = manager.findAll("LED", "GOJ");
 
-        assertArrayEquals(expexted, actual);
+        assertArrayEquals(expected, actual);
     }
+
+    @Test
+    void shouldSearchNoValues() {
+
+        repo.addTicket(ticket);
+        repo.addTicket(ticket1);
+        repo.addTicket(ticket2);
+        repo.addTicket(ticket3);
+
+        DataTicket[] expected = {};
+        DataTicket[] actual = manager.findAll("", "");
+
+        assertArrayEquals(expected, actual);
+
+    }
+
 }
